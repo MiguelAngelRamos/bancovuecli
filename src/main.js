39222210b62b90923d8fe73a3fb2e0adb1923fce
Miguel Angel Ramos <return.infox@gmail.com>
2022-02-28 20:37:28 -0300

    proyecto de banco vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// importar bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 createApp(App).mount('#app')
