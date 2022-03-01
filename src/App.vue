ac43c01218d381adf0487d6fcf1d0994e935da54
Miguel Angel Ramos <return.infox@gmail.com>
2022-02-28 21:03:42 -0300

    tabla

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <Navbar />
+  <div class="container mt-5 col-sm-6 offset-sm-3">
+    <Table/>
+  </div>
 </template>
 
 /* Instancia Vue */
 <script>
+
 import Navbar from './components/Navbar.vue';
+import Table from'./components/Table.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Table
   }
 }
 </script>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,20 @@
 <template>
-  
+
+  <table class="table">
+  <thead>
+    <tr>
+      <th>Saldo</th>
+      <th>Estado</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+    </tr>
+  </tbody>
+</table>
+
 </template>
 
 <script>
